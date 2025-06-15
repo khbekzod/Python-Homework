7af2ed37fdfa81dc73077bc851daf4f516638086
beka <khabibullaevbekzodk@gmail.com>
2025-06-15 05:05:11 +0000

    homework16 - added

---
+++ homework.16/task-1.py
@@ -0,0 +1,9 @@
+list1=(input("Please enter your East Coast cities: "))
+list2=(input("Please enter your West Coast cities: "))
+
+
+merge_lists = list1 + list2
+
+print(merge_lists)
+
+# I did research on how to do this on the Google, please let me know if there any other ways. Thank you. 
--- homework.16/task-3.py
+++ homework.16/task-3.py
@@ -1,1 +1,10 @@
-# hello
+name=(input("Please enter your name: "))
+
+if name == "Alice":
+    print("Hello, Alice!")
+elif name == "Bob":
+    print("Hello, Bob!")
+else:
+    print("Hello, guest!")
+
+    
